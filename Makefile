ebf836eaf5183820ec24b0536877fc1b7c285665
dev <dev@qiyutech.tech>
2022-01-24 22:52:53 +0800

    extract doc

--- Makefile
+++ Makefile
@@ -1,3 +1,7 @@
 
 format:
 	poetry run black *.py
+
+
+extract-ansible-doc:
+	poetry run python gen_ansible_doc.py docs
--- gen_ansible_doc.py
+++ gen_ansible_doc.py
@@ -1,13 +1,22 @@
 import json
+import os.path
 import subprocess
 
 import typer
 
 
-def main():
+def main(out_dir: str = typer.Argument(..., help="输出目录")):
+    """
+    获取 Ansible 所有模块的文档
+    """
     process = subprocess.run(["ansible-doc", "-l", "-j"], capture_output=True)
     out = json.loads(process.stdout)
-    print(out.keys())
+
+    for module in out.keys():
+        process = subprocess.run(["ansible-doc", module, "-j"], capture_output=True)
+        doc = json.loads(process.stdout)
+        with open(os.path.join(out_dir, f"{module}.json"), "w") as fp:
+            json.dump(doc, fp, ensure_ascii=False, indent=2)
 
 
 if __name__ == "__main__":
