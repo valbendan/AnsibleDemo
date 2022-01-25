30b4a14cceb2d9307d9146d439fa6a901ffebca9
dev <dev@qiyutech.tech>
2022-01-25 17:23:47 +0800

    v5

--- gen_ansible_doc.py
+++ gen_ansible_doc.py
@@ -13,7 +13,7 @@ class AnsibleModuleOption(BaseModel):
     description: List[str] = Field([])
     typ_: str = Field("str", alias="type")
     required: bool = Field(False)
-    default: Union[str, int, bool] = Field(None)
+    default: Union[str, int, bool, list] = Field(None)
     choices: List[Union[str, int]] = Field([])
     elements: str = Field("")
     version_added: str = Field("")
